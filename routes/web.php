47e72bda2ed27ee583148679e0dcbf5ce20bb17d
HaritriniainaJ <Haritriniainarabeharimanana@gmail.com>
2026-03-02 00:12:28 +0300

    Test: connectivite Discord

--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,15 @@ Route::get('/run-migrations', function () {
     Artisan::call('migrate', ['--force' => true]);
     return Artisan::output();
 });
+Route::get('/test-discord', function () {
+    try {
+        $response = \Illuminate\Support\Facades\Http::timeout(10)
+            ->get('https://discord.com/api/v10/gateway');
+        return response()->json(['status' => $response->status(), 'body' => $response->json()]);
+    } catch (\Exception $e) {
+        return response()->json(['error' => $e->getMessage()]);
+    }
+});
 
 Route::get('/', function () {
     return view('welcome');

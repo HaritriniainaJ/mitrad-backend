e7d0c318c29883510216f68d1ae94fef70b903cc
HaritriniainaJ <Haritriniainarabeharimanana@gmail.com>
2026-03-01 23:31:20 +0300

    Fix: CSRF except Discord routes

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,7 +3,6 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 use Illuminate\Http\Request;
-
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
@@ -15,6 +14,9 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->redirectGuestsTo(fn () => null);
         $middleware->web(append: [
             \Illuminate\Session\Middleware\StartSession::class,
+        ]);
+        $middleware->validateCsrfTokens(except: [
+            'auth/discord/*',
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {

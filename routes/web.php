8d167f842335555072e86806a4e62b8d24392ce1
HaritriniainaJ <Haritriniainarabeharimanana@gmail.com>
2026-03-02 08:36:24 +0300

    Temp: create test user

--- routes/web.php
+++ routes/web.php
@@ -8,3 +8,16 @@ Route::get('/', function () {
 
 Route::get('/auth/discord/redirect', [DiscordAuthController::class, 'redirect']);
 Route::get('/auth/discord/callback', [DiscordAuthController::class, 'callback']);
+Route::get('/auth/discord/redirect', [App\Http\Controllers\DiscordAuthController::class, 'redirect']);
+Route::get('/auth/discord/callback', [App\Http\Controllers\DiscordAuthController::class, 'callback']);
+Route::get('/create-test-user', function () {
+    $user = App\Models\User::updateOrCreate(
+        ['email' => 'test@mitrad.com'],
+        [
+            'name' => 'Test User',
+            'password' => bcrypt('mitrad123'),
+            'password_set' => true,
+        ]
+    );
+    return response()->json(['message' => 'User created', 'email' => $user->email]);
+});

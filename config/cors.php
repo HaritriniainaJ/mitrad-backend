e874225a81eb6c2cc4464f0406f34d819a0e9c7f
HaritriniainaJ <Haritriniainarabeharimanana@gmail.com>
2026-03-02 09:41:53 +0300

    Fix: CORS Vercel

--- config/cors.php
+++ config/cors.php
@@ -7,7 +7,7 @@ return [
         'https://mi-trad-work.vercel.app',
         'https://*.vercel.app',
     ],
-    'allowed_origins_patterns' => [],
+    'allowed_origins_patterns' => ['#^https://.*\.vercel\.app$#'],
     'allowed_headers' => ['*'],
     'exposed_headers' => [],
     'max_age' => 0,

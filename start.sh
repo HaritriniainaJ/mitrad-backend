b26d8a832d7dfb0bdc13b1a82ff61494a6bf7f86
HaritriniainaJ <Haritriniainarabeharimanana@gmail.com>
2026-03-04 14:26:41 +0300

    Fix: migrate non-bloquant au démarrage

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 ﻿#!/bin/sh
 php artisan config:clear
 php artisan cache:clear
-php artisan migrate --force
+php artisan migrate --force --no-interaction 2>/dev/null || true
 php artisan serve --host=0.0.0.0 --port=10000

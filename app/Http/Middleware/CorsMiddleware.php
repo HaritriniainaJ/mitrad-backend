a7c7513d7bcc273889ff9470c97707ea7a978b89
HaritriniainaJ <Haritriniainarabeharimanana@gmail.com>
2026-03-05 14:20:01 +0300

    Fix: CorsMiddleware dans bootstrap/app.php

---
+++ app/Http/Middleware/CorsMiddleware.php
@@ -0,0 +1,29 @@
+﻿<?php
+namespace App\Http\Middleware;
+use Closure;
+use Illuminate\Http\Request;
+
+class CorsMiddleware
+{
+    public function handle(Request $request, Closure $next)
+    {
+        $origin = $request->header('Origin');
+        $allowed = ['http://localhost:5173', 'http://localhost:8080', 'https://projournalmitrad.vercel.app'];
+
+        if (in_array($origin, $allowed) || str_ends_with($origin ?? '', '.vercel.app')) {
+            if ($request->isMethod('OPTIONS')) {
+                return response('', 200)
+                    ->header('Access-Control-Allow-Origin', $origin)
+                    ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
+                    ->header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Accept')
+                    ->header('Access-Control-Allow-Credentials', 'true');
+            }
+            return $next($request)
+                ->header('Access-Control-Allow-Origin', $origin)
+                ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
+                ->header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Accept')
+                ->header('Access-Control-Allow-Credentials', 'true');
+        }
+        return $next($request);
+    }
+}
--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,9 +1,8 @@
-<?php
+﻿<?php
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 use Illuminate\Http\Request;
-
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
@@ -12,6 +11,7 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
+        $middleware->prepend(\App\Http\Middleware\CorsMiddleware::class);
         $middleware->redirectGuestsTo(fn () => null);
         $middleware->web(append: [
             \Illuminate\Session\Middleware\StartSession::class,

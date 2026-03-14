d12c20b82f0a93670e6a03a3c537fd4ae733dfbf
HaritriniainaJ <Haritriniainarabeharimanana@gmail.com>
2026-03-14 23:58:03 +0300

    Revert: retour à l'état stable cc3aa52

--- app/Models/User.php
+++ app/Models/User.php
@@ -1,12 +1,16 @@
-﻿<?php
+<?php
+
 namespace App\Models;
+
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+
 class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
+
     protected $fillable = [
         'name',
         'email',
@@ -24,17 +28,18 @@ class User extends Authenticatable
         'favorite_pairs',
         'custom_setups',
     ];
+
     protected $hidden = [
         'password',
         'remember_token',
     ];
+
     protected $casts = [
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
         'password_set' => 'boolean',
-        'favorite_pairs' => 'array',
-        'custom_setups' => 'array',
     ];
+
     public function tradingAccounts()
     {
         return $this->hasMany(TradingAccount::class);

8c064adbbf0ee2faf145ebaeb9b2dc5ca486982c
HaritriniainaJ <Haritriniainarabeharimanana@gmail.com>
2026-03-14 23:38:57 +0300

    Fix: custom_setups et favorite_pairs castés en array

--- app/Models/User.php
+++ app/Models/User.php
@@ -1,16 +1,12 @@
-<?php
-
+﻿<?php
 namespace App\Models;
-
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
-
 class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
-
     protected $fillable = [
         'name',
         'email',
@@ -28,18 +24,17 @@ class User extends Authenticatable
         'favorite_pairs',
         'custom_setups',
     ];
-
     protected $hidden = [
         'password',
         'remember_token',
     ];
-
     protected $casts = [
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
         'password_set' => 'boolean',
+        'favorite_pairs' => 'array',
+        'custom_setups' => 'array',
     ];
-
     public function tradingAccounts()
     {
         return $this->hasMany(TradingAccount::class);
